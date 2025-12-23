ed389047e9f9bc17ff0c08d53de74eed62c60053
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2025-12-23 12:02:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click", () => {
+  const select = document.getElementById("colorSelect");
+
+  // Remove selected option
+  select.remove(select.selectedIndex);
+});
